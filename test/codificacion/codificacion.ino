4e927cb5331db3f9442ff5b09d42485c785e3b50
santiagogape <santiago.galindo101@alu.ulpgc.es>
2025-11-17 13:52:11 +0000

    me sacaron de la biblioteca

--- test/codificacion/codificacion.ino
+++ test/codificacion/codificacion.ino
@@ -3,8 +3,18 @@
 typedef struct {
   uint8_t bandwidth_index;
   uint8_t spreadingFactor;
+    /* [6, 12] Aumentar el spreading factor incrementa 
+    de forma significativa el tiempo de Tx
+    SPF = 6 es un valor especial
+    Ver tabla 12 del manual del SEMTECH SX1276 */
   uint8_t codingRate;
+  // [5, 8] 5 da un tiempo de Tx menor
   uint8_t txPower; 
+    /*
+    Rango [2, 20] en dBm
+    Importante seleccionar un valor bajo para pruebas
+    a corta distancia y evitar saturar al receptor
+    */
 } LoRaConfig_t;
 
 double bandwidth_kHz[10] = {7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3,
@@ -15,24 +25,26 @@ LoRaConfig_t remoteNodeConf = { 0,  0, 0, 0};
 int remoteRSSI = 0;
 float remoteSNR = 0;
 
-int randomRSSI() {return -random(0, 128);}
 
+// RSSI puede estar en un rango de [0, -127] dBm
+int randomRSSI() {return -random(0, 128);}
+uint8_t inRangeRSSI(int value){
+  if (value > -50) return 2; //excelente
+  else if(value < -80) return 1; //peor que bueno
+  else return 0; // bueno
+}
+
+// SNR puede estar en un rango de [20, -148] dBm
 float randomSNR() {return 20.0 - float(random(0, 101) / 100.0) * 168.0;}
+uint8_t inRangeRSSI(float value){
+  if (value > 10) return 2; //excelente
+  else if(value < 5) return 1; //peor que muy bueno
+  else return 0; // muy bueno
+}
 
 uint8_t package[50];
 
-/*
-    payload[payloadLength]    = (thisNodeConf.bandwidth_index << 4);
-    payload[payloadLength++] |= ((thisNodeConf.spreadingFactor - 6) << 1);
-    payload[payloadLength]    = ((thisNodeConf.codingRate - 5) << 6);
-    payload[payloadLength++] |= ((thisNodeConf.txPower - 2) << 1);
 
-    // Incluimos el RSSI y el SNR del último paquete recibido
-    // RSSI puede estar en un rango de [0, -127] dBm
-    payload[payloadLength++] = uint8_t(-LoRa.packetRssi() * 2);
-    // SNR puede estar en un rango de [20, -148] dBm
-    payload[payloadLength++] = uint8_t(148 + LoRa.packetSnr());
-*/
 
 uint8_t encode(LoRaConfig_t * node, int rssi, float snr, uint8_t *package, LoRaConfig_t * next_config){
   uint8_t length = 0;
